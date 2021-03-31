79a155ef62c70e4ada5fa8c72fbc421bca2593b2
wosanek <wosanek@gmail.com>
2021-03-31 10:51:55 +0600

    Добавлен MaxProgressValueConverter

---
+++ Dashboard/Converters/MaxProgressValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Dashboard.Converters
+{
+    public class MaxProgressValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            //var newProductName = string.Empty;
+            if (!(value is double currentValue))
+                    return value;
+            
+            return currentValue > 100 ? currentValue : 100;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
